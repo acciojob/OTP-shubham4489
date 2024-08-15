fc4689b7e782f1e8d12e3aeb0a1318a56b12c6a3
shubham4489 <143184073+shubham4489@users.noreply.github.com>
2024-08-15 14:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const codeContainer= document.querySelector(".code-container")
+const numberOfInputs = 6;
+let lastEnteredInputElement = null;
+
+function onInput(event){
+	const currentElement =event.target;
+	if(currentElement.value){
+		currentElement.nextSibling&&currentElement.nextSibling.focus();
+	}
+}
+
+function onBackSpace(e){
+	if(e.key === "Backspace" ){
+		e.target.value="";
+		e.target.previousElementSibling && e.target.previousElementSibling.focus();
+	}
+}
+	
+for(let i=1; i<=numberOfInputs; i++){
+	const input = document.createElement("input");
+	input.className = "code";
+	input.maxLength="1";
+	input.addEventListener("input" , onInput);
+	input.addEventListener("keyup", onBackSpace);
+	codeContainer.appendChild(input);
+}
+
+	
